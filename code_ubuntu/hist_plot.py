4e03e6f3726e01f899b8970a189a23ef58c01db9
kai aidan <kai@DESKTOP-ELMVTUT.localdomain>
2022-04-08 10:28:30 +0200

    updating python file

--- code_ubuntu/hist_plot.py
+++ code_ubuntu/hist_plot.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 s = pynbody.load('snapshot_data/snapshot_001') # Snapshot at z=0
 h = s.halos()
 m = []
-for i in range(1,len(h)):
+for i in tqdm(range(1,len(h)),desc='Collecting masses...'):
     m.append(h[i]['mass'].sum())
-n, bins, patches = plt.hist(m, bins=50, log=True, alpha=0.75, density=True, facecolor='green')
+n, bins, patches = plt.hist(m, bins=8, log=True, alpha=0.75, density=True, facecolor='green')
 plt.xlabel('Number of halos with this Mass')
 plt.ylabel('Probability')
 plt.title('Halo counts ')
