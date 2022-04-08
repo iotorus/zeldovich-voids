6892bb4c4c7d364e3f48eee9b0657f233ea7e102
kai aidan <kai@DESKTOP-ELMVTUT.localdomain>
2022-04-08 10:52:08 +0200

    updated python file

--- code_ubuntu/hist_plot.py
+++ code_ubuntu/hist_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pynbody
 import matplotlib.pyplot as plt
+import scipy.integrate as int
 
 # Load the snapshots
 s = pynbody.load('snapshot_data/snapshot_001') # Snapshot at z=0
@@ -9,9 +10,16 @@ m = []
 for i in tqdm(range(1,len(h)),desc='Collecting masses...'):
     m.append(h[i]['mass'].sum())
 n, bins, patches = plt.hist(m, bins=8, log=True, alpha=0.75, density=True, facecolor='green')
+
+print(n)
+print(bins)
+
+# N_exp = Vol*int.quad(n)
+
 plt.xlabel('Number of halos with this Mass')
 plt.ylabel('Probability')
 plt.title('Halo counts ')
+plt.legend()
 plt.grid(True)
 plt.show()
 plt.savefig('histogram.pdf')
